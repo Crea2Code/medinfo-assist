d28b4e899942748dbe7874768c22514253a5b853
Sonia CHALAL <contact@crea2code.fr>
2025-11-10 14:33:59 +0100

    Updates

--- _next/static/012SR-ROYlT_am_j42qdj/_buildManifest.js
+++ _next/static/012SR-ROYlT_am_j42qdj/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/medinfo-assist//_next/:path+",
-        "destination": "/medinfo-assist/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
